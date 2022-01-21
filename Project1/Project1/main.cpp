5f413a5bf8870589e412751d6a56125156eff60c
Ezra-Fikru Asfaw <1934089@edu.vaniercollege.qc.ca>
2022-01-21 08:37:33 -0500

    testing

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -16,7 +16,12 @@
 
 int main(void)
 {
-   
+    pthread_t thread_id;
+    printf("In main: creating a thread \n");
+    pthread_create(&thread_id, NULL, thread1, NULL);
+    Sleep(5000); // Blocks 5 seconds
+    printf("Main: returns! \n");
+
 }  
 
 
